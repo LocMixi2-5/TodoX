8bf53c11731efbbd1879c9642e2bf1148646f874
LocMixi2-5 <locmixi@gmail.com>
2026-03-09 11:30:25 +0700

    helo

--- backend/src/server.js
+++ backend/src/server.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
+  app.get("/{*splat}", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
